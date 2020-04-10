da1959c2bbb355b87ff9a54789e69d7199c07063
ryouji nagai <ryoujinagai@CBcloudnoMacBook-Pro.local>
2020-04-10 16:41:55 +0900

    finish 1st version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  
   get "/" => "home#top"
   get "about" => "home#about"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
