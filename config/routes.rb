e7ea3027e38c871eec2e111753c5de76eec35d6c
ryouji nagai <ryoujinagai@CBcloudnoMacBook-Pro.local>
2020-04-11 10:59:11 +0900

    add edit and delete function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
-  
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/destroy" => "posts#destroy"
+  post "posts/:id/update" => "posts#update"
   get "/" => "home#top"
   get "about" => "home#about"
 end
